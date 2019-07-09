37ccbbacd340ff4d080a60a7e735f2ff9a056573
M Chadwick <dr.m.chadwick@gmail.com>
2019-07-09 16:26:16 +0100

    Updated to use none fixed default dataset.

--- app.py
+++ app.py
@@ -25,7 +25,17 @@ def get_metadata_locations(path: Path) -> dict:
     return output
 
 
-metadata_dict = get_metadata_locations(Path('./static/plots'))
+metadata_dict = {}
+
+
+def update_meta_dict(index: Path = ('./index.json')):
+    with open(index, 'r') as f:
+        global metadata_dict
+        metadata_dict = json_load(f)
+
+    return
+
+
 data_summary_dir = Path('./static/data_summary')
 
 
@@ -36,38 +46,38 @@ def url_path(path_in: str, endpoint='static'):
     return url_for(endpoint, filename=path_in)
 
 
-def get_team_nav(team):
+def get_team_nav(team, dataset):
     '''Produces name, url_for pairs for a teams sidebar.
        To be used with the sidebar templates.
     '''
     navigators = [("Back", url_for("index"))]
-    navigators += [(team, url_for("team", team=team))]
+    navigators += [(team, url_for("team", team=team, dataset=dataset))]
     navigators += [("DIRE", None)]
     navigators += [("Drafts", url_for("serve_plots", team=team,
-                    side="dire", plot="draft"))]
+                    side="dire", plot="draft", dataset=dataset))]
     navigators += [("Wards", url_for("serve_plots", team=team,
-                    side="dire", plot="wards"))]
+                    side="dire", plot="wards", dataset=dataset))]
     navigators += [("Positioning", url_for("serve_plots", team=team,
-                    side="dire", plot="positioning"))]
+                    side="dire", plot="positioning", dataset=dataset))]
     navigators += [("Smokes", url_for("serve_plots", team=team,
-                    side="dire", plot="smoke"))]
+                    side="dire", plot="smoke", dataset=dataset))]
     navigators += [("Scans", url_for("serve_plots", team=team,
-                    side="dire", plot="scan"))]
+                    side="dire", plot="scan", dataset=dataset))]
 
     navigators += [("RADIANT", None)]
     navigators += [("Drafts", url_for("serve_plots", team=team,
-                    side="radiant", plot="draft"))]
+                    side="radiant", plot="draft", dataset=dataset))]
     navigators += [("Wards", url_for("serve_plots", team=team,
-                    side="radiant", plot="wards"))]
+                    side="radiant", plot="wards", dataset=dataset))]
     navigators += [("Positioning", url_for("serve_plots", team=team,
-                    side="radiant", plot="positioning"))]
+                    side="radiant", plot="positioning", dataset=dataset))]
     navigators += [("Smokes", url_for("serve_plots", team=team,
-                    side="radiant", plot="smoke"))]
+                    side="radiant", plot="smoke", dataset=dataset))]
     navigators += [("Scans", url_for("serve_plots", team=team,
-                    side="radiant", plot="scan"))]
+                    side="radiant", plot="scan", dataset=dataset))]
 
     navigators += [(None, None)]
-    navigators += [("Summary", url_for("summary", team=team))]
+    navigators += [("Summary", url_for("summary", team=team, dataset=dataset))]
 
     return navigators
 
@@ -98,7 +108,7 @@ def get_nav_report():
 
 def get_team_summary(team, dataset='default') -> dict:
     '''Returns a dictionary of summary plots with url_for links'''
-    with open(metadata_dict[team], 'r') as file:
+    with open(Path(metadata_dict[team]['path']), 'r') as file:
         json_file = json_load(file)
 
         if dataset not in json_file:
@@ -123,14 +133,14 @@ def render_plot_template(team, side, plot, dataset='default'):
     if plot not in ['draft', 'wards', 'positioning', 'smoke', 'scan']:
         abort(404)
 
-    with open(metadata_dict[team], 'r') as file:
+    with open(Path(metadata_dict[team]['path']), 'r') as file:
         json_file = json_load(file)
 
         if dataset not in json_file:
             abort(404)
 
         data = json_file[dataset]
-        navigators = get_team_nav(team)
+        navigators = get_team_nav(team, dataset)
         plots = {}
         if plot == 'draft':
             plots["drafts_link"] = "#{}_drafts".format(side)
@@ -180,9 +190,14 @@ def render_plot_template(team, side, plot, dataset='default'):
 
 @app.route("/")
 def index():
+    # Update our team index and datasets
+    update_meta_dict()
+    teams = list(metadata_dict.keys())
+    teams.sort()
     navigators = []
-    for team in metadata_dict:
-        url = url_for("team", team=team)
+    for team in teams:
+        url = url_for("team", team=team,
+                      dataset=metadata_dict[team]['sets'][0])
         navigators.append((team, url))
     # navigators.append((None, None))
     # navigators.append(("Summaries", url_for("data_summary")))
@@ -190,19 +205,24 @@ def index():
                            navigators=navigators)
 
 
-@app.route("/<string:team>/")
-def team(team, dataset='default'):
+@app.route("/<string:team>/<string:dataset>/")
+def team(team, dataset):
+    # Update our team index and datasets
+    update_meta_dict()
     team = unquote(team)
+    dataset = unquote(dataset)
     if team not in metadata_dict:
         abort(404)
-    with open(metadata_dict[team], 'r') as file:
+    if dataset not in metadata_dict[team]['sets']:
+        abort(404)
+    with open(Path(metadata_dict[team]['path']), 'r') as file:
         json_file = json_load(file)
 
         if dataset not in json_file:
             abort(404)
 
         data = json_file[dataset]
-        navigators = get_team_nav(team)
+        navigators = get_team_nav(team, dataset)
         dire = data['replays_dire']
         dire.sort(reverse=True)
         radiant = data['replays_radiant']
@@ -215,13 +235,14 @@ def team(team, dataset='default'):
                                winrates=data['stat_win_rate'])
 
 
-@app.route("/<string:team>/summary/")
-def summary(team):
+@app.route("/<string:team>/<string:dataset>/summary/")
+def summary(team, dataset='default'):
     team = unquote(team)
+    dataset = unquote(dataset)
     if team not in metadata_dict:
         abort(404)
-    navigators = get_team_nav(team)
-    summary = get_team_summary(team)
+    navigators = get_team_nav(team, dataset)
+    summary = get_team_summary(team, dataset)
     return render_template('plots/summary.j2',
                            navigators=navigators,
                            summary=summary,
@@ -251,12 +272,12 @@ def data_summary():
                            plots=plots)
 
 
-@app.route("/<string:team>/<string:side>/<string:plot>.html")
-def serve_plots(team, side, plot):
+@app.route("/<string:team>/<string:dataset>/<string:side>/<string:plot>.html")
+def serve_plots(team, dataset, side, plot):
     team = unquote(team)
     if team not in metadata_dict:
         abort(404)
-    return render_plot_template(team, side, plot)
+    return render_plot_template(team, side, plot, dataset=dataset)
 
 
 @app.route("/<string:team>/report/")
