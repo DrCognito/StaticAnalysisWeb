83ada33f87edfe1e0ef29039cca3e313850869be
Matthew Chadwick <dr.m.chadwick@gmail.com>
2021-12-22 17:38:17 +0000

    Copy script

---
+++ copy.ps1
@@ -0,0 +1,7 @@
+$FAST_COPY = "D:\ToolStrap\FastCopyPortable\FastCopyPortable.exe"
+$DEST = "E:\Python\StaticAnalysisWeb\static\plots\"
+$ORIGIN = "E:\Python\StaticAnalysis\plots\*"
+$JOB = "PlotCopy"
+$OPTIONS = "/job=$JOB"
+
+& $FAST_COPY $OPTIONS /no_ui | echo "Waiting"
