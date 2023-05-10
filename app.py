52f0c2ed3094f209cad3b931be612007ab4e96ad
Matthew Chadwick <dr.m.chadwick@gmail.com>
2023-05-10 20:36:12 +0100

    Added priority picks to summary.

--- app.py
+++ app.py
@@ -194,7 +194,6 @@ def get_team_summary(team, dataset="default", postfix="") -> dict:
 
         data = json_file[dataset]
 
-
         summary_dict = {}
         summary_dict["draft_summary"] = url_path(data.get(f"plot_draft_summary{postfix}"))
         summary_dict["hero_picks"] = url_path(data.get(f"plot_hero_picks{postfix}"))
@@ -203,6 +202,8 @@ def get_team_summary(team, dataset="default", postfix="") -> dict:
         summary_dict["pick_context"] = url_path(data.get(f"plot_pick_context{postfix}"))
         summary_dict["win_rate"] = url_path(data.get(f"plot_win_rate{postfix}"))
         summary_dict["rune"] = url_path(data.get(f"plot_rune_control{postfix}"))
+        summary_dict["pick_priority_first"] = url_path(data.get(f"pick_priority_first"))
+        summary_dict["pick_priority_second"] = url_path(data.get(f"pick_priority_second"))
         if postfix == "":
             summary_dict["pick_table"] = url_path(data.get("plot_picktables", None))
         else:
