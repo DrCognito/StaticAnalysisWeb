97d3575055796a256722ca5268df1b13fc6907ac
Matthew Chadwick <dr.m.chadwick@gmail.com>
2023-05-23 23:41:02 +0100

    Count images before we start loading as it occurs async so wont line up. When all images are loaded paste into our canvas. Fix for counting height and widths correctly.

--- templates/plots/draft.js
+++ templates/plots/draft.js
@@ -7,7 +7,17 @@ function add_image(draft, canv) {
     img.src = url;
     img.id = draft;
     img.canv = canv;
-    img.onload = img_onload(img);
+    // This has to be in function and use "this" or img will be cached as an unloaded obj it seems.
+    img.onload =  function () {
+        var canv = this.canv;
+        loaded_count[canv]++;
+        max_height[canv] += this.height;
+        max_width[canv] = Math.max(max_width[canv], this.width);
+
+        if(loaded_count[canv] === r_count[canv]){
+            draw_drafts(canv);
+        }
+    };
     // document.body.appendChild(img);
     return img;
 }
@@ -23,21 +33,32 @@ function add_canvas(canv_id){
 }
 
 function img_onload(img){
-    var canv = img.canv;
+    var canv = this.canv;
     loaded_count[canv]++;
-    max_height[canv] = Math.max(max_height[canv], img.height);
-    max_width[canv] = Math.max(max_width[canv], img.width);
+    max_height[canv] += this.height;
+    max_width[canv] = Math.max(max_width[canv], this.width);
 
-    if(loaded_count[canv] === sheets[canv].length){
+    if(loaded_count[canv] === r_count[canv]){
         draw_drafts(canv);
     }
 }
 
 function draw_drafts(canv){
-    console.log("This is where we draw: " + canv);
-    console.log("Max X: " + max_width[canv] + " Max Y: " + max_height[canv]);
+    // console.log("This is where we draw: " + canv);
+    // console.log("Max X: " + max_width[canv] + " Max Y: " + max_height[canv]);
+    var spacing = 0;
+    canvas[canv].height = max_height[canv] + 13*spacing;
+    canvas[canv].width = max_width[canv];
+
+    const ctx = canvas[canv].getContext("2d");
+    for(let i = 0, y = 0; i < sheets[canv].length; i++){
+        let img = sheets[canv][i];
+        ctx.drawImage(img, 0, y);
+        y += img.height + spacing;
+    }
 }
 
+const per_sheet = 13;
 var replays = {{plots["plot_drafts"]}};
 var sheets = new Map();
 var canvas =  new Map();
@@ -48,9 +69,20 @@ var loaded_count = new Map();
 canvas["canv_0"] = add_canvas("canv_0");
 sheets["canv_0"] = new Array();
 loaded_count["canv_0"] = 0;
-max_height["canv_0"] = -Infinity;
-max_width["canv_0"] = -Infinity;
+max_height["canv_0"] = 0;
+max_width["canv_0"] = 0;
 console.log("Draft.js");
+
+// Do this in advance to ensure that we await the propper amount of replays
+// There might be a better way to do this but lets not over think it.
+var n_replays = replays.length/per_sheet;
+var r_count = new Map();
+for(let i = 0; i < Math.floor(n_replays); i++){
+    var canv = "canv_" + i;
+    r_count[canv] = per_sheet;
+}
+r_count["canv_" + Math.floor(n_replays)] = replays.length % per_sheet;
+
 
 for (let i = 0, len = replays.length, s = 0, j = 0; i < len; i++, j++){
     var canv = "canv_" + s;
@@ -64,7 +96,7 @@ for (let i = 0, len = replays.length, s = 0, j = 0; i < len; i++, j++){
         sheets[canv] =  new Array();
         canvas[canv] = add_canvas(canv);
         loaded_count[canv] = 0;
-        max_height[canv] = -Infinity;
-        max_width[canv] = -Infinity;
+        max_height[canv] = 0;
+        max_width[canv] = 0;
     }
 }
